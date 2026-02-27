09755dfcc2b850753034338aabb348b89420d963
Shawn <18322761430@163.com>
2026-02-27 22:21:03 +0800

    Update from Lovable (2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./contexts/AuthContext";
 import SwipeBackWrapper from "./components/SwipeBackWrapper";
